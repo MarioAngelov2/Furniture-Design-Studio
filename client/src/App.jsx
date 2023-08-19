82df0f59f860759b022905baac10b5d4f1edd4ab
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-19 14:28:20 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Header />
+      <Projects />
     </div>
   )
 }
