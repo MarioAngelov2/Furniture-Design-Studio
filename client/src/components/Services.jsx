2f8e5cfa1bb766b30012fb9f0f5c6a974af85d7e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-19 15:54:17 +0300

    Create service component

---
+++ client/src/components/Services.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import project2 from "../assets/project2.jpeg";
+import project3 from "../assets/project3.jpeg";
+
+function Services() {
+    return (
+        <div className="md:grid grid-cols-2 max-w-[1440px] mx-auto mt-20">
+            <div className="bg-[#F6F4F1] h-[591px] md:h-auto flex">
+                <div className="flex flex-col justify-center align-middle px-20">
+                    <h1 className="text-3xl mb-6">Elegant flow</h1>
+                    <p>
+                        A great design tells a story. Discover оur story and
+                        design аpproach to creating flawless interiors.
+                    </p>
+                    <p className="underline underline-offset-4 mt-6">
+                        ABOUT US
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img
+                        className="aspect-square object-cover"
+                        src={project2}
+                        alt=""
+                    />
+                </div>
+            </div>
+            {/* section2 */}
+            <div>
+                <div>
+                    <img
+                        className="aspect-square object-cover"
+                        src={project3}
+                        alt=""
+                    />
+                </div>
+            </div>
+            <div className="bg-[#F6F4F1] h-[591px] md:h-auto flex">
+                <div className="flex flex-col justify-center align-middle px-20">
+                    <h1 className="text-3xl mb-6">Redefining Luxury</h1>
+                    <p>
+                        Luxury in interior design has evolved, and redefining
+                        its meaning is at the heart of our philosophy and design
+                        approach.
+                    </p>
+                    <p className="underline underline-offset-4 mt-6">
+                        VIEW SERVICES
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Services;
