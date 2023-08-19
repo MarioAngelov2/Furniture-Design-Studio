a0d7c9fee97a68f85b4f9c6fd3c1b039737cd785
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-19 13:19:23 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar />
+      <Header />
     </div>
   )
 }
