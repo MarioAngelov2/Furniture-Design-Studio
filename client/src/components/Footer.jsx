c02d47d8f7aa73b4740fda57e86b724d09b37a4a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-20 19:59:18 +0300

    Minor responsive refactor

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,16 +11,16 @@ import {
 function Footer() {
     return (
         <div className="max-w-[1440px] mx-auto mt-28">
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 px-12  mb-4">
-                <div className="mb-5 flex flex-col items-center justify-between">
+            <div className="grid grid-cols-1 place-items-center md:grid md:grid-cols-3 md:place-items-start lg:grid-cols-5 px-12 mb-4">
+                <div className="flex flex-col items-center gap-4 md:flex md:flex-col md:mt-10 md:items-start">
                     <img className="w-[130px] h-[117px]" src={logo} alt="" />
                     <div className="flex flex-row gap-2 text-[#888681]">
-                        <BiLogoFacebook size={22}/>
-                        <BiLogoInstagram size={22}/>
-                        <BiLogoLinkedin size={22}/>
+                        <BiLogoFacebook size={24} />
+                        <BiLogoInstagram size={24} />
+                        <BiLogoLinkedin size={24} />
                     </div>
                 </div>
-                <div className="mb-5">
+                <div className="flex flex-col items-center mt-10 md:mb-5 md:grid md:place-items-start">
                     <h3 className="mb-3 text-lg tracking-widest">PAGES</h3>
                     <ul className="text-sm font-thin space-y-2">
                         <li>Services</li>
@@ -30,7 +30,7 @@ function Footer() {
                         <li>Contact</li>
                     </ul>
                 </div>
-                <div className="mb-5">
+                <div className="flex flex-col items-center mt-10 md:mb-5 md:grid md:place-items-start">
                     <h3 className="mb-3 text-lg tracking-widest">
                         USEFUL LINKS
                     </h3>
@@ -41,7 +41,7 @@ function Footer() {
                         <li>Journal</li>
                     </ul>
                 </div>
-                <div className="mb-5">
+                <div className="flex flex-col items-center mt-10 md:mb-5 md:grid md:place-items-start">
                     <h3 className="mb-3 text-lg tracking-widest">CONTACTS</h3>
                     <ul className="text-sm font-thin space-y-2">
                         <li className="flex items-center gap-2">
@@ -58,15 +58,15 @@ function Footer() {
                         </li>
                     </ul>
                 </div>
-                <div className="mb-5">
+                <div className="flex flex-col items-center mt-10 md:mb-5 md:grid md:place-items-start">
                     <h3 className="mb-3 text-lg tracking-widest">NEWSLETTER</h3>
                     <label className="text-sm font-thin">Email address</label>
                     <input
-                        className="border w-full h-[20%] border-black mt-1"
+                        className="border w-full h-[100%] border-black mt-1"
                         type="email"
                     />
                     <div className="flex justify-end">
-                        <button className="mt-5 underline underline-offset-8 tracking-widest">
+                        <button className="text-sm md:text-md  mt-5 underline underline-offset-8 tracking-widest">
                             SUBSCRIBE
                         </button>
                     </div>
@@ -86,65 +86,3 @@ function Footer() {
 }
 
 export default Footer;
-
-// <div className="flex flex-col md:grid grid-cols-5 max-w-[1440px] mx-auto mt-28 px-12 mb-4">
-//             <div className="flex flex-col items-center md:flex md:items-start justify-center">
-//                 <img className="w-[130px] h-[117px]" src={logo} alt="" />
-//             </div>
-//             <div className="flex flex-col items-center mt-8 md:flex md:flex-col md:items-start gap-2">
-//                 <h2 className="text-lg mb-0 md:text-lg tracking-widest md:mb-4">
-//                     PAGES
-//                 </h2>
-//                 <div className="text-sm flex flex-col gap-2">
-//                     <p>Services</p>
-//                     <p>Portfolio</p>
-//                     <p>About</p>
-//                     <p>Shop</p>
-//                     <p>Contact</p>
-//                 </div>
-//             </div>
-//             <div className="flex flex-col items-center mt-8 md:flex md:flex-col md:items-start gap-2">
-//                 <h2 className="text-lg md:tracking-widest md:mb-4">
-//                     USEFUL LINKS
-//                 </h2>
-//                 <div className="text-sm flex flex-col gap-2">
-//                     <p>Privacy policy</p>
-//                     <p>Cookie policy</p>
-//                     <p>Press</p>
-//                     <p>Journal</p>
-//                 </div>
-//             </div>
-//             <div className="flex flex-col items-center mt-8 md:flex md:flex-col md:items-start gap-2">
-//                 <h2 className="text-lg tracking-widest md:mb-4">CONTACTS</h2>
-//                 <div className="text-sm flex flex-col gap-2">
-//                     <div className="flex gap-2">
-//                         <AiOutlineMail size={24} />
-//                         studio@elinorinteriors.com
-//                     </div>
-//                     <div className="flex gap-2">
-//                         <LiaPhoneSolid size={24} />
-//                         0881122345 / Sofia, Bulgaria
-//                     </div>
-//                     <div className="flex gap-2">
-//                         <LiaPhoneSolid size={24} />
-//                         0881122345 / New York, USA
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className="flex flex-col items-center mt-8 md:flex md:flex-col md:items-start gap-2">
-//                 <h2 className="text-lg tracking-widest mb-4">NEWSLETTER</h2>
-//                 <label className="text-sm">Email Address</label>
-//                 <input
-//                     className="border items-center w-[70%] h-[40px] md:w-[100%] md:h-[20%]"
-//                     type="email"
-//                     placeholder="ivan@gmail.com"
-//                 />
-//                 <div className="mt-4 flex items-end">
-//                     <button className="underline underline-offset-8 tracking-widest">
-//                         SUBSCRIBE
-//                     </button>
-//                 </div>
-//             </div>
-//             <div className=" w-[1440px]">
-//             </div>
-//         </div>
