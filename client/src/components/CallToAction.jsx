88f699f655f5ddb11498f8b1e493997f256cfe92
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-20 17:11:25 +0300

    Minor refactor to make it responsive

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function CallToAction() {
     return (
-        <div className="flex justify-center items-center m-w-[1440px] h-[576px] bg-[#F6F4F1] mt-20">
-            <div className="flex flex-col items-center gap-6 w-[35%]">
-                <h1 className="text-3xl text-center">
+        <div className="flex justify-center md:flex md:justify-center items-center md:max-w-[1440px] h-[576px] mx-auto bg-[#F6F4F1] mt-20">
+            <div className="flex flex-col w-[60%] md:flex md:flex-col items-center gap-6 md:w-[35%]">
+                <h1 className="text-2xl md:text-3xl text-center">
                     Redefining minimalist design for your own home
                 </h1>
                 <p className="text-center text-sm tracking-wider">
@@ -12,7 +12,7 @@ function CallToAction() {
                     tranquility, elevate functionality, and reflect the unique
                     personality and values of our clients.
                 </p>
-                <p className="text-center mt-2 underline underline-offset-8 tracking-widest">SEND AN ENQUIRE</p>
+                <p className="text-sm md:text-center mt-2 underline underline-offset-8 tracking-widest">SEND AN ENQUIRE</p>
             </div>
         </div>
     );
