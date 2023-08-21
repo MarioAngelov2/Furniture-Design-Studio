31582be24c6b14cffea5746457bd010803e66f58
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 11:23:50 +0300

    Create shopItems.js

---
+++ client/src/utils/shopItems.js
@@ -0,0 +1,18 @@
+import item1 from "../assets/item1.jpeg";
+import item2 from "../assets/item2.jpeg";
+import item3 from "../assets/item3.jpeg";
+
+export const shopItems = [
+    {
+        title: "VINTAGE CHAIR",
+        image: item1,
+    },
+    {
+        title: "VINTAGE CHAIR",
+        image: item2,
+    },
+    {
+        title: "VINTAGE CHAIR",
+        image: item3,
+    },
+];
