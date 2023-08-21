7f5dff33c0181e89c547f73e7f71ad215c38d928
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 21:13:03 +0300

    Add more schema data

--- server/src/models/Blog.js
+++ server/src/models/Blog.js
@@ -4,8 +4,10 @@ const BlogSchema = new mongoose.Schema({
     images: [String],
     title: String,
     article: String,
+    userPostDate: String,
+    readTime: String,
 });
 
-const BlogModel = mongoose.model("Shop", BlogSchema);
+const BlogModel = mongoose.model("Blog", BlogSchema);
 
 module.exports = BlogModel;
