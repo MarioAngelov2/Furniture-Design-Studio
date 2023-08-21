8ec63d6a1fb609204fea9c0c3b2674022f4a3808
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 16:07:45 +0300

    Add projects route

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const getProducts = require("../controllers/getProducts");
+const getProjects = require("../controllers/getProjects");
 
 const router = Router();
 
 router.get("/products", getProducts);
+router.get("/projects", getProjects);
 
 module.exports = router;
