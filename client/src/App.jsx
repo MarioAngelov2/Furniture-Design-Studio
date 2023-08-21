77f57fa2305deb9cab5ee11ea8928ebfeaf5da3a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 10:23:05 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import './App.css'
-import Blog from './components/Blog'
-import CallToAction from './components/CallToAction'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Projects from './components/Projects'
-import Services from './components/Services'
-import Shop from './components/Shop'
+import { useRef } from "react";
+import "./App.css";
+import Blog from "./components/Blog";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
+import Shop from "./components/Shop";
 
 function App() {
-
-  return (
-    <div>
-      <Navbar />
-      <Header />
-      <Projects />
-      <Services />
-      <Shop />
-      <Blog />
-      <CallToAction />
-      <Footer />
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <Header />
+            <Projects />
+            <Services />
+            <Shop />
+            <Blog />
+            <CallToAction />
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
