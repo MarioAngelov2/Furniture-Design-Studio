d25a20409db575f5ec3dbb2edd21c0c4f24ccdf4
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 16:08:01 +0300

    Fetch projects from db  & refactor

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,51 +1,57 @@
-import React from "react";
-import project1 from "../assets/project1.jpeg";
-import project2 from "../assets/project2.jpeg";
+import React, { useState, useEffect } from "react";
+import * as requester from "../api/requester";
 
 function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    async function loadProjects() {
+        try {
+            const response = await requester.getProjects();
+            setProjects(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        loadProjects();
+    }, []);
+
+    const displayProjects =
+        projects.length > 0 &&
+        projects.map((project) => (
+            <div key={project._id} className="flex justify-center">
+                <div className="px-12">
+                    <img
+                        className="aspect-square object-cover w-full md:h-[701px]"
+                        src={project.images}
+                        alt=""
+                    />
+                    <div className="flex flex-col items-start gap-1 mb-10 mt-2 md:flex md:flex-row align-middle justify-between md:text-center md:mt-4">
+                        <h3 className="text-lg md:text-start md:text-xl">
+                            {project.title}
+                        </h3>
+                        <p className="text-xs md:text-md md:text-base underline underline-offset-8 tracking-widest">
+                            VIEW DETAILS
+                        </p>
+                    </div>
+                </div>
+            </div>
+        ));
+
     return (
         <>
-            <div id="portfolio" className="md:flex justify-between max-w-[1440px] mx-auto items-center px-12 mt-20 md:mb-8">
+            <div
+                id="portfolio"
+                className="md:flex justify-between max-w-[1440px] mx-auto items-center px-12 mt-20 md:mb-8"
+            >
                 <h1 className="mb-2 text-3xl md:text-3xl">Latest Projects</h1>
                 <p className="underline underline-offset-8 text-xs md:text-base tracking-widest">
                     SEE PORTFOLIO
                 </p>
             </div>
             <div className="md:grid grid-cols-2 max-w-[1440px] mx-auto text-center mt-10">
-                <div className="flex justify-center">
-                    <div className="px-12">
-                        <img
-                            className="aspect-square object-cover w-full md:h-[701px]"
-                            src={project1}
-                            alt=""
-                        />
-                        <div className="flex flex-col items-start gap-1 mb-10 mt-2 md:flex md:flex-row align-middle justify-between md:text-center md:mt-4">
-                            <h3 className="text-lg md:text-start md:text-xl">
-                                Project Tintyava
-                            </h3>
-                            <p className="text-xs md:text-md md:text-base underline underline-offset-8 tracking-widest">
-                                VIEW DETAILS
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex justify-center">
-                    <div className="px-12">
-                        <img
-                            className="aspect-square object-cover w-full md:h-[701px]"
-                            src={project2}
-                            alt=""
-                        />
-                        <div className="flex flex-col items-start gap-1 mb-10 mt-2 md:flex md:flex-row align-middle justify-between text-center md:mt-4">
-                            <h3 className="text-lg md:text-start md:text-xl">
-                                Project Mandur
-                            </h3>
-                            <p className="text-xs md:text-end md:text-base underline underline-offset-8 tracking-widest">
-                                VIEW DETAILS
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                {displayProjects}
             </div>
         </>
     );
