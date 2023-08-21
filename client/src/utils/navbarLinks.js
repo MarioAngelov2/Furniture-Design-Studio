06bf397304d5c888e39cf3f9f78403a242a525e2
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 11:23:48 +0300

    Create navbarLinks.js

---
+++ client/src/utils/navbarLinks.js
@@ -0,0 +1,39 @@
+import logo from "../assets/logo_dark.png";
+
+
+export const navbarLinks = [
+    {
+        title: "SERVICES",
+        target: "services",
+    },
+    {
+        title: "PORTFOLIO",
+        target: "portfolio",
+    },
+    {
+        title: "ABOUT",
+        target: "about",
+    },
+    {
+        title: "LOGO",
+        isLogo: true,
+        imageSrc: logo,
+        altText: "Logo",
+    },
+    {
+        title: "SHOP",
+        target: "shop",
+    },
+    {
+        title: "JOURNAL",
+        target: "blog",
+    },
+    {
+        title: "PRESS",
+        target: "press",
+    },
+    {
+        title: "CONTACT",
+        target: "contact",
+    },
+];
