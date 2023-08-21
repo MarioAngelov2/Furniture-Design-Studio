3b852a2829ce574ad8b0cd866f2a806a39b13cfd
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 13:32:39 +0300

    Add global route path

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
-const express = require('express')
-const cors = require('cors');
-const { default: mongoose } = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const router = require("./src/routes/routes");
 
 const app = express();
 
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+mongoose.connect(process.env.MONGO_CONNECTION_STRING);
 
-app.get('/', (req, res) => {
-    res.json('works')
-})
+app.use("/", router);
 
-app.listen(4002, () => console.log('Server is listening on port 4002...'))
+app.listen(4002, () => console.log("Server is listening on port 4002..."));
