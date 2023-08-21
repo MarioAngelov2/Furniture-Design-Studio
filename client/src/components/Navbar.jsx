8f10a747092f098263475cd86f4130204b07f88e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 11:23:43 +0300

    Refactor & optimase code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-scroll";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { AiOutlineClose } from "react-icons/ai";
 import { BiLogoFacebook, BiLogoInstagram } from "react-icons/bi";
+import { navbarLinks } from "../utils/navbarLinks";
 
 import logo from "../assets/logo_dark.png";
 
@@ -17,73 +18,32 @@ function Navbar() {
         <>
             <div className="flex fixed right-0 left-0 bg-white h-[90px] md:bg-white md:h-[100px] py-4 justify-between max-w-[1440px] mx-auto items-center md:flex-col px-4">
                 <div className="absolute hidden md:left-10 md:top-2 md:flex items-start gap-2">
-                    <BiLogoFacebook size={18}/>
-                    <BiLogoInstagram size={18}/>
+                    <BiLogoFacebook size={18} />
+                    <BiLogoInstagram size={18} />
                 </div>
                 <div className="absolute right-3 top-3 md:right-10 md:top-2 flex items-start gap-2">
                     <p className="text-xs tracking-widest">BG/EN</p>
                 </div>
                 <ul className="hidden md:flex gap-5 items-center text-sm ">
-                    <li className="p-4 cursor-pointer">
-                        <Link
-                            to="services"
-                            spy={true}
-                            smooth={true}
-                            offset={-100}
-                            duration={500}
-                        >
-                            SERVICES
-                        </Link>
-                    </li>
-                    <li className="p-4 cursor-pointer">
-                        <Link
-                            to="portfolio"
-                            spy={true}
-                            smooth={true}
-                            offset={-100}
-                            duration={500}
-                        >
-                            PORTFOLIO
-                        </Link>
-                    </li>
-                    <li className="p-4">ABOUT</li>
-                    <div>
-                        <img className="w-20" src={logo} alt="" />
-                    </div>
-                    <li className="p-4 cursor-pointer">
-                        <Link
-                            to="shop"
-                            spy={true}
-                            smooth={true}
-                            offset={-100}
-                            duration={500}
-                        >
-                            SHOP
-                        </Link>
-                    </li>
-                    <li className="p-4 cursor-pointer">
-                        <Link
-                            to="blog"
-                            spy={true}
-                            smooth={true}
-                            offset={-100}
-                            duration={500}
-                        >
-                            JOURNAL
-                        </Link>
-                    </li>
-                    <li className="p-4">PRESS</li>
-                    <li className="p-4 cursor-pointer">
-                        <Link
-                            to="contact"
-                            spy={true}
-                            smooth={true}
-                            offset={-100}
-                            duration={500}
-                        >
-                            CONTACT
-                        </Link>
-                    </li>
+                    {navbarLinks.map((link) => (
+                        <li className="p-4 cursor-pointer">
+                            {link.isLogo ? (
+                                <div className="mx-auto">
+                                    <img className="w-16" src={link.imageSrc} />
+                                </div>
+                            ) : (
+                                <Link
+                                    to={link.targe}
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-100}
+                                    duration={500}
+                                >
+                                    {link.title}
+                                </Link>
+                            )}
+                        </li>
+                    ))}
                 </ul>
                 <div className="w-[40%] h-[20%] bg-white">
                     <div onClick={handleNav} className="block md:hidden">
@@ -112,63 +72,19 @@ function Navbar() {
                     }
                 >
                     <ul className="text-sm my-2 cursor-pointer">
-                        <li className="p-4">
-                            <Link
-                                to="services"
-                                spy={true}
-                                smooth={true}
-                                offset={-100}
-                                duration={500}
-                            >
-                                SERVICES
-                            </Link>
-                        </li>
-                        <li className="p-4">
-                            <Link
-                                to="portfolio"
-                                spy={true}
-                                smooth={true}
-                                offset={-100}
-                                duration={500}
-                            >
-                                PORTFOLIO
-                            </Link>
-                        </li>
-                        <li className="p-4">ABOUT</li>
-                        <li className="p-4">
-                            <Link
-                                to="shop"
-                                spy={true}
-                                smooth={true}
-                                offset={-100}
-                                duration={500}
-                            >
-                                SHOP
-                            </Link>
-                        </li>
-                        <li className="p-4">
-                            <Link
-                                to="blog"
-                                spy={true}
-                                smooth={true}
-                                offset={-100}
-                                duration={500}
-                            >
-                                JOURNAL
-                            </Link>
-                        </li>
-                        <li className="p-4">PRESS</li>
-                        <li className="p-4">
-                            <Link
-                                to="contact"
-                                spy={true}
-                                smooth={true}
-                                offset={50}
-                                duration={500}
-                            >
-                                CONTACT
-                            </Link>
-                        </li>
+                        {navbarLinks.map((link) => (
+                            <li className="p-4">
+                                <Link
+                                    to={link.target}
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-100}
+                                    duration={500}
+                                >
+                                    {link.title}
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
