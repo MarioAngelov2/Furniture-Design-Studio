34b11065d1917fbe33e90ade7b87c15e076c847f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-21 21:13:07 +0300

    Update routes.js

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const getProducts = require("../controllers/getProducts");
 const getProjects = require("../controllers/getProjects");
+const getBlogPosts = require("../controllers/getBlogPosts");
+const insertSampleData = require("../controllers/insertSampleData");
 
 const router = Router();
 
 router.get("/products", getProducts);
 router.get("/projects", getProjects);
+router.get('/posts', getBlogPosts)
 
 module.exports = router;
