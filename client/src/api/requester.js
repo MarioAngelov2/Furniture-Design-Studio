481dab6688847240a3ccc90e7bcded4f2617d417
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-22 15:36:57 +0300

    Centralize the response handling logic

--- client/src/api/requester.js
+++ client/src/api/requester.js
@@ -1,12 +1,22 @@
 const URL = "http://localhost:4002";
 
+async function handleResponse(response) {
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    return response.json();
+}
+
 export async function getProducts() {
     try {
-        const response = await fetch(`${URL}/products`);
-
-        if (response.ok) {
-            return response.json();
-        }
+        const response = await fetch(`${URL}/products`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return handleResponse(response);
     } catch (error) {
         console.log(error);
     }
@@ -14,25 +24,26 @@ export async function getProducts() {
 
 export async function getProjects() {
     try {
-        const response = await fetch(`${URL}/projects`);
-
-        if (response.ok) {
-            return response.json();
-        }
+        const response = await fetch(`${URL}/projects`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return handleResponse(response);
     } catch (error) {
         console.log(error);
     }
 }
 
-
 export async function getBlogPosts() {
     try {
-        const response = await fetch(`${URL}/posts`);
-
-        if (response.ok) {
-            return response.json();
-        }
+        const response = await fetch(`${URL}/posts`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return handleResponse(response);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
