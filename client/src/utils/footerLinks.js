e55289788c08474a5576da72272449e017dad584
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-22 11:42:53 +0300

    Create footerLinks.js

---
+++ client/src/utils/footerLinks.js
@@ -0,0 +1,40 @@
+import { AiOutlineMail } from "react-icons/ai";
+import { LiaPhoneSolid } from "react-icons/lia";
+
+export const pagesLink = [
+    {
+        title: "Services",
+        target: "services",
+    },
+    {
+        title: "Portfolio",
+        target: "portfolio",
+    },
+    {
+        title: "About",
+        target: "about",
+    },
+    {
+        title: "Shop",
+        target: "shop",
+    },
+    {
+        title: "Contact",
+        target: "blog",
+    },
+];
+
+export const usefulLinks = [
+    {
+        title: "Privacy policy",
+    },
+    {
+        title: "Cookie policy",
+    },
+    {
+        title: "Press",
+    },
+    {
+        title: "Journal",
+    },
+];
